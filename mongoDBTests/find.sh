e6aa22f18cb36a289475a0f7c0e2c38357d89902
Nathan <nwolf2@oswego.edu>
2022-03-23 14:06:44 -0400

    created folder for bash curl requests to test mongodb tests

---
+++ mongoDBTests/find.sh
@@ -0,0 +1,9 @@
+curl --location --request POST 'https://data.mongodb-api.com/app/data-rlgbq/endpoint/data/beta/action/find' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header 'api-key: TUGyzJPmesVH4FcrDqO0XovgYNq0L5B59xCnjFsB9nLFE7qkofdTvzYjBn2ID120' \
+--data-raw '{
+    "collection":"Jars",
+    "database":"TikoJarTest",
+    "dataSource":"PositivityJar"
+}' | json_pp
