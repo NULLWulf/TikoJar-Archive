ce21d09f68e65364fc46038c13f7aef7d0843fb8
Nathan <nwolf2@oswego.edu>
2022-03-28 13:23:57 -0400

    added delte message bash query

---
+++ mongoDBTests/deleteMessage.sh
@@ -0,0 +1,10 @@
+curl --location --request POST 'https://data.mongodb-api.com/app/data-rlgbq/endpoint/data/beta/action/updateOne' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header 'api-key: TUGyzJPmesVH4FcrDqO0XovgYNq0L5B59xCnjFsB9nLFE7qkofdTvzYjBn2ID120' \
+--data-raw '{"collection":"Jars",
+             "database":"TikoJarTest",
+             "dataSource":"PositivityJar",
+             "filter": { "serverID": "956306200063008830" },
+             "update": { 
+                 "$pull": { "messages.$.messageId" : "VDFajqgcOQ" }} | json_pp
